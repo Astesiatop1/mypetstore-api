7f864859c51e8510f2f08abb2c4a4930e02d1007
spiderman <2101430956@qq.com>
2022-04-09 15:30:58 +0800

    初始化连接

---
+++ src/main/java/org/csu/mypetstore/api/MypetstoreApiDemoApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreApiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/mypetstore/api/MypetstoreApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstore.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
